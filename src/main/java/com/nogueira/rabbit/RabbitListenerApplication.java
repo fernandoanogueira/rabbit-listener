75e4d5d6c637f4259c3eb5d9fdf680f0b2292b44
Fernando Nogueira <fernandoanogueira@gmail.com>
2022-03-17 00:49:08 -0300

    Initial commit

---
+++ src/main/java/com/nogueira/rabbit/RabbitListenerApplication.java
@@ -0,0 +1,13 @@
+package com.nogueira.rabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitListenerApplication.class, args);
+	}
+
+}
